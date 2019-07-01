432cedd3bab5cf29a653f53847a0f04d50af1fc3
Hans Goor <rooteddev@outlook.com>
2019-07-01 21:08:29 +0200

    Added failsafe for credentials not existing

--- do_the_thing.py
+++ do_the_thing.py
@@ -1,7 +1,18 @@
+import os
+
 from api_handlers.google import get_drive_files
+from utils.filehandler import get_path
+from utils.logger import Logger
+
+LOGGER = Logger(logger_name="Drive2Teams ; Main", filename=None)
 
 
 def main():
+    if not os.path.isfile(get_path("auth", "credentials.json")):
+        LOGGER.fatal("Cannot find credentials.json in directory auth. Please generate these via the Google Developer Console first.")
+        LOGGER.fatal("It can be found here: https://console.developers.google.com/")
+        exit(1)
+
     get_drive_files()
 
 
